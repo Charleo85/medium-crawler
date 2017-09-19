80fa9048b96d2d66a9aadba60758ea94c0ac94ea
Charlie Wu <wujibang@gmail.com>
2017-09-19 00:58:03 -0400

    clean up deprecated file

---
+++ scripts/clean_up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# rm -rf data/article/*
+# rm -rf data/comment/*
+# rm -rf data/truth/*
+# rm -rf cache/html/*
+# rm -rf cache/json/*
+# rm -rf cache/variable/*
+# rm -rf cache/logs/*
+
+# rm -rf data/*
+# rm -rf cache/*
